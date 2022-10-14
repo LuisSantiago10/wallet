98a793c574d8b36492c70468493dfa3129d89794
luis <lux_luis10@hotmail.com>
2022-10-14 14:23:53 -0500

    inital test practice

---
+++ src/services/movement.service.spec.ts
@@ -0,0 +1,18 @@
+import assert from 'assert';
+
+describe('Movement.Service',() =>{
+    describe('Store', () =>{
+        it('tries to register an income movement', () =>{
+
+        });
+        it('tries to register an outcome movement', () =>{
+
+        });
+        it('tries to register an aoutcome movement with insufficient balance', () =>{
+
+        });
+        it('tries to registeran unexpected movement', () =>{
+
+        });
+    })
+})
